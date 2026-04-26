6b21a54d8ba4daa2263c677f9749314f7e0e6fd2
Pedro Meira <pedrohmeira@icloud.com>
2026-04-26 20:39:22 -0300

    adicionar mapeie_bytes_para_codigos (não finalizado)

--- comprimir.c
+++ comprimir.c
@@ -3,6 +3,7 @@
 #include "functions/levante_frequencia_dos_bytes.h"
 #include "functions/ordene_bytes_na_arvore.h"
 #include "functions/construa_arvore_de_huffman.h"
+#include "functions/mapeie_bytes_para_codigos.h"
 
 ArvoreHuffman* comprimir(FILE* arquivo) {
     int tamanho;
@@ -11,5 +12,6 @@ ArvoreHuffman* comprimir(FILE* arquivo) {
     free(frequencias);
     ArvoreHuffman* arvore = construaArvoreDeHuffman(fila);
     destruirFilaPrioridade(fila);
+    Codigo* tabela = mapeieByteParaCodigos(arvore);
     return arvore;
 }
---
+++ functions/mapeie_bytes_para_codigos.h
@@ -0,0 +1,9 @@
+#ifndef MAPEIE_BYTES_PARA_CODIGOS_H
+#define MAPEIE_BYTES_PARA_CODIGOS_H
+
+#include "../huffman.h"
+#include "../codigo.h"
+
+Codigo* mapeieByteParaCodigos(ArvoreHuffman* arvore);
+
+#endif
